74dfdbf08f010f47270b22956fef5f9a0bed603d
Petr Baudis <pasky@ucw.cz>
2014-03-04 03:26:10 +0100

    population.py: Record detailed stepping information to .mdat file

--- population.py
+++ population.py
@@ -4,8 +4,12 @@
 """
 A generic manager of iteratively minimized population that will
 keep track of solutions and respective minimizer states.
+
+Solution search progress (in respect to method population) is
+recorded to an .mdat file.
 """
 
+import os;
 import string
 import sys
 import time
@@ -15,6 +19,35 @@ import numpy as np
 import numpy.random as nr
 
 from cocopf.minstep import MinimizeStepping
+
+
+class SteppingData:
+    def __init__(self, pop):
+        self.pop = pop
+
+        # XXX: This is evil; copied from beginning of fgeneric.evalfun()
+        if not pop.fi.f._is_setdim or pop.fi.f._dim != pop.fi.dim:
+            pop.fi.f._setdim(pop.fi.dim)
+        if not pop.fi.f._is_ready():
+            pop.fi.f._readytostart()
+
+        self.datafile = open(os.path.splitext(pop.fi.f.datafile)[0] + '.mdat', 'a')
+        self.datafile.write("% function evaluation | portfolio iteration | instance index | instance method | instance invocations | instance best noise-free fitness - Fopt | best noise-free fitness - Fopt | x1 | x2...\n")
+    def record(self, i):
+        f = self.pop.fi.f
+        e = f.lasteval
+        res = ('%d %d %d %s %d %+10.9e %+10.9e'
+               % (e.num, self.pop.total_iters,
+                  i, self.pop.methods[i].name, self.pop.iters[i],
+                  self.pop.values[i] - f.fopt, e.bestf - f.fopt))
+
+        tmp = []
+        for x in self.pop.points[i]:
+            tmp.append(' %+5.4e' % x)
+        res += ''.join(tmp)
+
+        self.datafile.write(res + '\n')
+        self.datafile.flush()
 
 
 class Population:
@@ -38,6 +71,7 @@ class Population:
 
         self.total_steps = 0
         self.total_iters = 0
+        self.data = SteppingData(self);
 
     def _minimizer_make(self, i):
         warnings.simplefilter("ignore") # ignore warnings about unused/ignored options
@@ -86,6 +120,7 @@ class Population:
         self.values[i] = y
         self.iters[i] += 1
         self.total_steps += 1
+        self.data.record(i)
         return (x, y)
 
     def restart_one(self, i):
